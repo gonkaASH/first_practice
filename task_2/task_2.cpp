32f0152ccb4ea9d2458b8d28d28d27883958f00e
gonkaASH <gonka121333555@gmail.com>
2026-06-16 11:41:08 +0800

    Ввод и вывод данных по ТЗ (задача 2)

--- task_2/task_2.cpp
+++ task_2/task_2.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 #include <vector>
 
-//Класс графа
+// Класс графа
 class Graph {
   int count_vertex = 0;
   std::vector<std::vector<int>> edges;
 
  public:
-  //Конструктор преобразования
+  // Конструктор преобразования
   Graph(int v) : count_vertex(v), edges(v) {
   }
 
-  //Добавление ребра
+  // Добавление ребра
   void AddEdge(int from, int to) {
     edges[from].push_back(to);
     edges[to].push_back(from);
   }
-  
-  //Получение смежных вершин
+
+  // Получение смежных вершин
   std::vector<int> GetChildren(int v) {
     return edges[v];
   }
 
-  //Обход в глубину
+  // Обход в глубину
   void DFS(int v, std::vector<int>& visited) {
     visited[v] = 1;
     std::vector<int> children = GetChildren(v);
@@ -36,16 +36,20 @@ class Graph {
 
 int main() {
 
-  int n = 5;
+  int n, m;
+  std::cin >> n >> m;
+
   Graph graph = n;
   std::vector<int> visited(n, 0);
 
-  int cnt_components = 0;
+  //Чтение графа
+  int to, from;
+  for (int i = 0; i < m; ++i) {
+    std::cin >> from >> to;
+    graph.AddEdge(from - 1, to - 1);
+  }
 
-  graph.AddEdge(1, 2);
-  graph.AddEdge(0, 4);
-  graph.AddEdge(0, 3);
-  graph.AddEdge(3, 4);
+  int cnt_components = 0;
 
   for (int i = 0; i < n; ++i) {
     if (visited[i] == 0) {
@@ -54,5 +58,5 @@ int main() {
     }
   }
 
-  std::cout << cnt_components;
+  std::cout << cnt_components - 1;
 }
