8c634f64c0957dc3a5b30fecb393013594172b3b
gonkaASH <gonka121333555@gmail.com>
2026-06-18 19:55:52 +0800

    Задание решено (6)

---
+++ task_6/task_6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+
+int main() {
+  int n, a, b, x, y;
+  std::cin >> n;
+
+  // Максимальное количество всех пар и пар с микроконтрролеррами типа B
+  int sum_pairs, max_pairs_B;
+
+  for (int i = 0; i < n; ++i) {
+    sum_pairs = max_pairs_B = 0;
+    std::cin >> a >> b >> x >> y;
+
+    max_pairs_B = std::min(b, x);  // Подсчёт числа пар с микроконтроллерами типа B
+    b -= max_pairs_B;              // Вычет микроконтроллеров (B), уже использованных
+    x -= max_pairs_B;              // Вычет модулей управления (1), уже использованных
+    sum_pairs += max_pairs_B;      // Прибавка к общей сумме
+
+    // Формируем пары из оставшихся модулей контроля и микроконтроллеров типа B
+    sum_pairs += std::min(x + y, a);
+
+    std::cout << sum_pairs << "\n";
+  }
+}
