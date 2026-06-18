874f755d71566ee40e97026dcecb29c084173a1e
gonkaASH <gonka121333555@gmail.com>
2026-06-18 16:23:01 +0800

    Оптимизация решения (4)

--- task_4/task_4.cpp
+++ task_4/task_4.cpp
@@ -1,45 +1,43 @@
 #include <iostream>
 #include <vector>
-
-// Вычисление обратной перестановки (для декодирования)
-std::vector<int> Inverse(std::vector<int>& p) {
-  int n = p.size();
-  std::vector<int> inv_p = p;
-  for (int i = 0; i < p.size(); ++i) {
-    inv_p[p[i] - 1] = i + 1;
-  }
-  return inv_p;
-}
-
-//Применение перестановки к строке
-std::string Permutation(std::string& s, std::vector<int>& p) {
-  int n = p.size();
-  std::string res = s;
-  for (int i = 0; i < n; ++i) {
-    res[i] = s[p[i] - 1];
-  }
-  return res;
-}
+#include <string>
 
 int main() {
 
-  int n, k, a;
-  std::vector<int> permutation;
+  int n, k;
   std::string s;
 
   std::cin >> n >> k;
+
+  std::vector<int> permutation(n, 0);
   for (int i = 0; i < n; ++i) {
-    std::cin >> a;
-    permutation.push_back(a);
+    std::cin >> permutation[i];
+    --permutation[i];
   }
   std::cin >> s;
 
-  std::vector<int> inv_permutation = Inverse(permutation);
+  std::string res(n, ' ');
+  std::vector<bool> visited(n, false);
 
-  //k раз применяем обратную перестановку для декодировки
-  for (int i = 0; i < k; ++i) {
-    s = Permutation(s, inv_permutation);
+  for (int i = 0; i < n; i++) {
+    if (!visited[i]) {
+      // Находим цикл
+      std::vector<int> cycle;
+      int cur = i;
+      while (!visited[cur]) {
+        visited[cur] = true;
+        cycle.push_back(cur);
+        cur = permutation[cur];
+      }
+
+      // Восстанавливаем символы
+      int len = cycle.size();
+      int shift = k % len;
+
+      for (int j = 0; j < len; j++) {
+        res[cycle[j]] = s[cycle[(j - shift + len) % len]];
+      }
+    }
   }
-
-  std::cout << s << "\n";
+  std::cout << res << "\n";
 }
