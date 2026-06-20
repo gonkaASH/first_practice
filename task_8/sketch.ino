516b4d9b5c00a55c53b3dda1f66e7f0eac657a91
gonkaASH <gonka121333555@gmail.com>
2026-06-20 16:58:40 +0800

    Проверка работы матрицы

--- task_8/sketch.ino
+++ task_8/sketch.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Adafruit_NeoPixel.h>
 
+#define PIN 3
+#define NUMPIXELS 64 //8x8 светодиодов
+
+// Инициализируем матрицу
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  pixels.begin();
+  pixels.setBrightness(40); // Яркость
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // По очереди зажигаем светодиоды зеленым цветом
+  for(int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 255, 0)); // Зеленый
+    pixels.show();
+    delay(30); // Скорость бега огонька (в миллисекундах)
+  }
 
+  delay(500); // Небольшая пауза
+
+  // Так же по очереди их гасим
+  for(int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // Выключить цвет
+    pixels.show();
+    delay(30);
+  }
+
+  delay(500);
 }
