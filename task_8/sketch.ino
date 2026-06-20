dff820b3c1399a9a62b0bdfa72882cf0637172f4
gonkaASH <gonka121333555@gmail.com>
2026-06-20 19:10:28 +0800

    Анимация с расходящимися квадратами

--- task_8/sketch.ino
+++ task_8/sketch.ino
@@ -1,32 +1,115 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 3
-#define NUMPIXELS 64 //8x8 светодиодов
+#define NUMPIXELS 64
 
-// Инициализируем матрицу
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   pixels.begin();
-  pixels.setBrightness(40); // Яркость
+  pixels.setBrightness(40);
+  pixels.show();
 }
 
+// Наша математическая функция "змейки"
+int getLedIndex(int x, int y) {
+  if (x < 0 || x > 7 || y < 0 || y > 7) return -1; 
+  if (y % 2 == 0) {
+    return (y * 8) + 7 - x;
+  } else {
+    return (y * 8) + x;
+  }
+}
+
+// Функция-обертка для рисования
+void drawPixel(int x, int y, uint32_t color) {
+  int index = getLedIndex(x, y);
+  if (index != -1) {
+    pixels.setPixelColor(index, color);
+  }
+}
+
+// Функция для отрисовки картинки на матрице
+void drawFrame(uint8_t frame[8][8], uint32_t color) {
+  pixels.clear();
+  for(int y = 0; y < 8; y++) {
+    for(int x = 0; x < 8; x++) {
+      if (frame[y][x] == 1) { 
+        drawPixel(x, y, color); // Зажигаем только те, где прописана единица
+      }
+    }
+  }
+  pixels.show(); // Отправляем данные на матрицу
+}
+
+uint8_t frame1[8][8] = {
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,1,1,0,0,0},
+  {0,0,0,1,1,0,0,0},
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0}
+};
+
+uint8_t frame2[8][8] = {
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0},
+  {0,0,1,1,1,1,0,0},
+  {0,0,1,0,0,1,0,0},
+  {0,0,1,0,0,1,0,0},
+  {0,0,1,1,1,1,0,0},
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0}
+};
+
+uint8_t frame3[8][8] = {
+  {0,0,0,0,0,0,0,0},
+  {0,1,1,1,1,1,1,0},
+  {0,1,0,0,0,0,1,0},
+  {0,1,0,0,0,0,1,0},
+  {0,1,0,0,0,0,1,0},
+  {0,1,0,0,0,0,1,0},
+  {0,1,1,1,1,1,1,0},
+  {0,0,0,0,0,0,0,0}
+};
+
+uint8_t frame4[8][8] = {
+  {1,1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1}
+};
+
+uint8_t frame5[8][8] = {
+  {1,1,1,1,1,1,1,1},
+  {1,1,1,1,1,1,1,1},
+  {1,1,1,1,1,1,1,1},
+  {1,1,1,1,1,1,1,1},
+  {1,1,1,1,1,1,1,1},
+  {1,1,1,1,1,1,1,1},
+  {1,1,1,1,1,1,1,1},
+  {1,1,1,1,1,1,1,1}
+};
+
 void loop() {
-  // По очереди зажигаем светодиоды зеленым цветом
-  for(int i = 0; i < NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(0, 255, 0)); // Зеленый
-    pixels.show();
-    delay(30); // Скорость бега огонька (в миллисекундах)
-  }
+  drawFrame(frame1, pixels.Color(255, 255, 0));
+  delay(500);
 
-  delay(500); // Небольшая пауза
+  drawFrame(frame2, pixels.Color(255, 255, 0));
+  delay(500);
 
-  // Так же по очереди их гасим
-  for(int i = 0; i < NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // Выключить цвет
-    pixels.show();
-    delay(30);
-  }
+  drawFrame(frame3, pixels.Color(255, 255, 0));
+  delay(500);
 
+  drawFrame(frame4, pixels.Color(255, 255, 0));
+  delay(500);
+
+  drawFrame(frame5, pixels.Color(255, 255, 0));
   delay(500);
 }
