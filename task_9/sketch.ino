8aa1f07c48a541c15c0903751a7d0a448a7abe5c
gonkaASH <gonka121333555@gmail.com>
2026-06-24 21:46:05 +0800

    Рабочий код автоматической двери

--- task_9/sketch.ino
+++ task_9/sketch.ino
@@ -1,9 +1,44 @@
-void setup() {
-  // put your setup code here, to run once:
+const int redLedPin = 9;
+const int greenLedPin = 5;
+const int sensorPin = A0;
 
+bool isDoorOpen = false;
+
+void setup() {
+  pinMode(redLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
+  
+  // Инициализация последовательного порта для вывода сообщений
+  Serial.begin(9600); 
+
+  // По умолчанию горит красный светодиод (дверь закрыта)
+  digitalWrite(redLedPin, HIGH);
+  digitalWrite(greenLedPin, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Чтение значения с фоторезистора
+  int sensorValue = analogRead(sensorPin);
 
+  if (sensorValue > 512) {
+    if (!isDoorOpen) {
+      digitalWrite(redLedPin, LOW);
+      digitalWrite(greenLedPin, HIGH);
+      Serial.println("Двери открываются!");
+
+      isDoorOpen = true;
+
+      delay(3000); //Перерыв 3 секунды после открытия двери
+    } 
+  } else {
+    if (isDoorOpen) {
+      digitalWrite(greenLedPin, LOW);
+      digitalWrite(redLedPin, HIGH);
+      Serial.println("Двери закрываются!");
+
+      isDoorOpen = false;
+    } 
+  }
+  
+  delay(100); 
 }
